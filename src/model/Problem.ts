04ae90d57be85b5f90d8c55a5a72f31253832a95
hannahellis4242 <hannahellis4242@gmail.com>
2023-10-06 23:44:51 +0100

    adding tests for api

--- src/model/Problem.ts
+++ src/model/Problem.ts
@@ -2,8 +2,12 @@ import { z } from "zod";
 import { SignatureSizeAndCountSchema } from "./SignatureSizeAndCount";
 
 export const ProblemSchema = z
-  .array(SignatureSizeAndCountSchema)
-  .nonempty({ message: "must contain at least one element" });
+  .array(SignatureSizeAndCountSchema, {
+    description: "A list of Signature sizes and counts",
+    invalid_type_error: "The problem must be an array",
+    required_error: "The problem is required",
+  })
+  .nonempty({ message: "The problem must contain at least one signature" });
 
 type Problem = z.infer<typeof ProblemSchema>;
 export default Problem;
